f1d1d2537bc00ff881f50251ce316deb230f2c46
Xaxis <william.neeley@gmail.com>
2014-03-25 07:35:11 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,101 @@
-jquery.eye
-==========
+# jQuery Eye
+
+Version 1.0.0
+
+## Summary
+
+jQuery.eye is a jQuery plugin for monitoring changes made to elements' DOM or CSS properties as well monitoring changes of the returned results from jQuery methods ran on a given element. When a change is detected a callback function is fired. Additionally provided are methods for pausing a watch, starting a watch, and retrieving a watch status associated with an element.
+
+## Author
+
+Wil Neeley ( [@wilneeley](http://twitter.com/wilneeley) / [trestlemedia.net](http://www.trestlemedia.net) / [github.com](https://github.com/Xaxis) )
+
+## Usage
+
+Include `jquery.eye.min.js` after jQuery in your header or elsewhere in your page.
+
+### Initialize Watches w/ jQuery.eye
+
+```javascript
+// Register element(s) to watch DOM and CSS properties
+$('.some-element').eye({
+  'clientWidth': function( oldVal, newVal, elm ) {
+    $(elm).html('clientHeight changed from ' + oldVal + ' to ' + newVal);
+  },
+  'background-color': function( oldVal, newVal, elm ) {
+    $(elm).html('background-color changed from ' + oldVal + ' to ' + newVal);
+  },
+  'border-color': {
+      onChange: function( oldVal, newVal, elm, args ) {
+          console.log('Border Color Changed!');
+      },
+      onInterval: function( currValue, elm, args ) {
+
+          // Execute any code you want EVERY interval of the watch
+          $(elm).css('border-color', '#' + Math.floor(Math.random()*16777215).toString(16));
+      }
+  }
+}, 100);
+
+
+// Register the element to watch and the properties to watch
+$('.some-div').eye({
+    'width()': function( oldVal, newVal, elm ) {
+        $(elm).prepend('<div>DIV\'s jQuery.width() return value changed from <strong>' + oldVal + '</strong> to <strong>' + newVal + '</strong></div>');
+    },
+    'attr()': {
+        args: ['border'],
+        onChange: function( oldVal, newVal, elm ) {
+            $(elm).prepend('<div>DIV\'s jQuery.attr(\'border\') return value changed from <strong>' + oldVal + '</strong> to <strong>' + newVal + '</strong></div>');
+        }
+    }
+}, 300);
+
+
+// Watch for the addition of the class 'some-class' to the DIV
+$('.some-div').eye({
+    speed: 250,
+    'hasClass()': {
+        args: ['some-class'],
+        onChange: function( oldVal, newVal, elm, args ) {
+            $(elm).prepend('<div>DIV\'s hasClass(\'some-class\') return value changed from <strong>' + oldVal + '</strong> to <strong>' + newVal + '</strong></div>');
+        }
+    }
+});
+```
+
+### Using Eye Methods
+
+```javascript
+// Pause a watch
+$('.some-element').eye('pause');
+
+// Re-start a paused watch
+$('.some-element').eye('start');
+
+// Return the status of a watched element
+$('.some-element').eye('status');
+
+// Remove a watched property from a watch
+$('.some-div').eye('unwatch', 'border-color');
+```
+
+### Caveats
+
+The jQuery.eye plugin uses JavaScript's setInterval method to monitor registered property values of DOM elements. While tests will vary from environment to environment, it is always wise to consider performance issues when registering a "large number" of watches.
+
+## Requirements/Browsers
+
+Tested with jQuery 1.4.x+.
+
+Works in IE6+, Chrome 14+, Safari 4+, Firefox 3.0+, Opera 10+.
+
+## Examples
+
+See `example.html` in examples folder.
+
+### Changelog
+
+#### Version 1.0
+
+* initial version
---
+++ jquery.eye.js
@@ -0,0 +1,308 @@
+/**
+ * jQuery.eye()
+ *
+ * (a) Wil Neeley, Trestle Media, LLC.
+ * Code may be freely distributed under the MIT license.
+ */
+;(function ( $, window, document, undefined ) {
+
+
+  var
+
+      // The plugin's name
+      plugin_name = 'eye',
+
+      // Ref to plugin itself
+      plugin_ref = null,
+
+      // The plugin's defaults
+      defaults = {
+        speed: 100
+      },
+
+      // The plugin's globals
+      globals = {
+
+        // A place to store references to eyeed elements
+        registry: {},
+
+        // A starting unique ID
+        guid: 0
+      };
+
+  // Plugin constructor
+  function Plugin( element, options, speed ) {
+    this.element = element;
+    this.options = $.extend( {}, defaults, options);
+    this._defaults = defaults;
+    this._name = plugin_name;
+    this._speed = speed;
+    this.init();
+  }
+
+  Plugin.prototype.init = function() {
+    var plugin = this;
+
+    // A place to reference our properties to watch and our watching interval
+    var watched_props = {
+      _interval: null,
+      _interval_func: null,
+      _element: plugin.element,
+      _speed: plugin._speed || plugin.options.speed,
+      _status: 'active'
+    };
+
+    // Build an object containing the properties to watch
+    $.each(plugin.options, function(index, value) {
+      if (index != 'speed') {
+        var prop_object = {
+          orig_object: value,
+          orig_value: '',
+          elm_ref: plugin.element
+        };
+
+        // Are we returning a jQuery function value?
+        if (index.match(/\)/g)) {
+          var j_func = index.replace(/\(\)/g, "");
+
+          // Are there arguments to pass to the function
+          if ($.isPlainObject(value) && 'args' in value) {
+
+            // If function is method of jQuery
+            if (j_func in $) {
+              value.args.unshift(plugin.element);
+              prop_object.orig_value = $[j_func].apply(j_func, value.args);
+
+            // Assume function is method of jQuery.fn
+            } else {
+              prop_object.orig_value = $.fn[j_func].apply($(plugin.element), value.args);
+            }
+          } else {
+
+            // If function is method of jQuery
+            if (j_func in $) {
+              prop_object.orig_value = $(plugin.element)[j_func]();
+
+            // Assume function is method of jQuery.fn
+            } else {
+              prop_object.orig_value = $.fn[j_func].apply($(plugin.element));
+            }
+          }
+
+        // Are we watching a property of the DOM element
+        } else if (index in plugin.element) {
+          prop_object.orig_value = plugin.element[index];
+
+        // Assume we are watching a CSS property?
+        } else {
+          var index_before = index;
+
+          // Convert comma separated CSS property to its camel case equivalent
+          index = index.replace(/-[a-zA-Z0-9]{1}/g, function(txt) {
+            return txt.charAt(1).toUpperCase();
+          });
+
+          // Assign the element style property
+          if (index in plugin.element.style) {
+            prop_object.orig_value = plugin.element.style[index];
+
+          // If this test fails, we can report that the property also does not exist on the element
+          } else {
+             console.log('Invalid Element Property: \'' + index_before + '\' does not exist.');
+          }
+        }
+
+        // Register the watched properties
+        watched_props[index] = prop_object;
+      }
+    });
+
+    // Reference the interval function
+    var intervalFunc = function() {
+      $.each(watched_props, function(index, value) {
+        if (index != '_interval' && index != '_interval_func' && index != '_element' && index != '_speed' && index != '_status') {
+          var new_value = value.orig_value;
+
+          // Are we returning a jQuery function value?
+          if (index.match(/\)/g)) {
+            var j_func = index.replace(/\(\)/g, "");
+
+            // Do we have arguments to pass to the function
+            if ($.isPlainObject(value) && 'args' in value.orig_object) {
+
+              // If function is method of jQuery
+              if (j_func in $) {
+                new_value = $[j_func].apply(j_func, value.orig_object.args);
+
+              // Assume function is method of jQuery.fn
+              } else {
+                new_value = $.fn[j_func].apply($(plugin.element), value.orig_object.args);
+              }
+            } else {
+
+              // If function is method of jQuery
+              if (j_func in $) {
+                new_value = $(plugin.element)[j_func]();
+
+              // Assume function is method of jQuery.fn
+              } else {
+                new_value = $.fn[j_func].apply($(plugin.element));
+              }
+            }
+
+          // Are we watching a property of the DOM element
+          } else if (index in plugin.element) {
+            new_value = plugin.element[index];
+
+          // Are we watching a CSS property?
+          } else {
+            new_value = plugin.element.style[index];
+          }
+        }
+
+        // Execute the interval callback when present
+        if ($.isPlainObject(value.orig_object)) {
+          if ('onInterval' in value.orig_object) {
+            value.orig_object.onInterval( value.orig_value, value.elm_ref);
+          }
+        }
+
+        // When the currently calculated value differs from the original, execute callback
+        if (new_value != value.orig_value) {
+          if ($.isFunction(value.orig_object)) {
+            value.orig_object( value.orig_value, new_value, value.elm_ref );
+          } else if ($.isPlainObject(value.orig_object)) {
+            value.orig_object.onChange( value.orig_value, new_value, value.elm_ref, 'args' in value.orig_object ? value.orig_object.args : [] );
+          }
+
+          // Update the original value with the new value
+          value.orig_value = new_value;
+        }
+      });
+    };
+
+    // Now register a watching interval on the properties of the element
+    watched_props._interval = setInterval(intervalFunc, watched_props._speed);
+    watched_props._interval_func = intervalFunc;
+
+    // Reference the current unique ID
+    var unique_id = globals.guid;
+
+    // Update the unique IDs associated with the current element(s)
+    if (!$.data(plugin.element, 'uids')) {
+      $.data(plugin.element, 'uids', [unique_id]);
+    } else {
+      var uids = $.data(plugin.element, 'uids');
+      uids.push(unique_id);
+      $.data(plugin.element, 'uids', uids);
+    }
+
+    // Increment the unique ID, keeping it unique for next watch registration
+    globals.guid++;
+
+    // Add this batch of properties to watch to the registry
+    globals.registry[unique_id] = watched_props;
+  };
+
+  // Public methods
+  var public_methods = {
+
+    /**
+     * Returns a string indicative of the status of a watch attached to an element/object. Will return 'active',
+     * 'paused', or 'none' depending on watch status.
+     * @param elm
+     * @returns {string}
+     */
+    status: function( elm ) {
+      var uids = elm.data('uids');
+      if (uids) {
+        var ret = '';
+        $.each(uids, function(index, value) {
+          var prop_object = globals.registry[value];
+          ret = prop_object._status;
+          return false;
+        });
+        return ret;
+      } else {
+        return 'none';
+      }
+    },
+
+    /**
+     * Pauses a watch of any registered watches on elements/objects.
+     * @param elm
+     */
+    pause: function( elm ) {
+      var uids = elm.data('uids');
+      if (uids) {
+        $.each(uids, function(index, value) {
+          var prop_object = globals.registry[value];
+          prop_object._status = 'paused';
+          clearInterval(prop_object._interval);
+        });
+      }
+      return plugin_ref;
+    },
+
+    /**
+     * Re-starts a watch of any registered watched on elements/objects.
+     * @param elm
+     */
+    start: function( elm ) {
+      var uids = elm.data('uids');
+      if (uids) {
+        $.each(uids, function(index, value) {
+          var prop_object = globals.registry[value];
+          if (prop_object._status == 'paused') {
+            prop_object._interval = setInterval(prop_object._interval_func, prop_object._speed);
+          }
+          prop_object._status = 'active';
+        });
+      }
+      return plugin_ref;
+    },
+
+    /**
+     * Removes a registered/watched property on an element
+     */
+    unwatch: function( elm, prop ) {
+
+      // Convert targeted CSS property to its camel case equivalent
+      prop = prop.replace(/-[a-zA-Z0-9]{1}/g, function(txt) {
+        return txt.charAt(1).toUpperCase();
+      });
+
+      var uids = elm.data('uids');
+      if (uids) {
+        $.each(uids, function(index, value) {
+          var prop_object = globals.registry[value];
+
+          // Remove the property from the watched properties object
+          delete prop_object[prop];
+        });
+      }
+      return plugin_ref;
+    }
+  };
+
+  // Plugin wrapper around constructor
+  $.fn[plugin_name] = function ( options, speed ) {
+
+    // Reference the plugin globally
+    plugin_ref = this;
+
+    // Call methods or init plugin as appropriate
+    if (typeof options == 'string') {
+      var method_name = options;
+      var args = $(arguments).toArray();
+      args.shift();
+      args.unshift(this);
+      return public_methods[method_name].apply(this, args);
+    } else {
+      return this.each(function () {
+        $.data(this, 'plugin_' + plugin_name, new Plugin( this, options, speed ));
+      });
+    }
+  };
+
+})( jQuery, window, document );
